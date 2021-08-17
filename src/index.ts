14b6ad1bdee143281233a98df9fba1a18f033979
mike <kimyoungmichael2@gmail.com>
2021-08-16 21:13:22 -0400

    first commit; installed exaclibur and set it up with parcel. Tested Actors, collisions, and basic keyboard API

---
+++ src/index.ts
@@ -0,0 +1,104 @@
+// Excalibur is loaded into the ex global namespace
+import { Actor, Engine, Color, CollisionType } from 'excalibur'
+
+// ./src/index.ts
+const game = new Engine({
+    width: 800,
+    height: 400,
+  })
+
+const mike = new Actor({
+    x: 800,
+    y: 150,
+    width:50,
+    height:50,
+});
+
+const han = new Actor({
+    x: 300,
+    y: 300,
+    width: 50,
+    height: 50,
+})
+
+mike.color = Color.Orange;
+han.body.useCircleCollider(50);
+han.color = Color.Black;
+
+// game.input.pointers.primary.on("move", (evt) => {
+//     mike.pos.x = evt.worldPos.x;
+//     mike.pos.y = evt.worldPos.y;
+// })
+
+game.input.keyboard.on("press", (evt) => {
+    if (evt.key === 37) {
+        mike.pos.x = mike.pos.x - 5;
+    }
+    if (evt.key === 38) {
+        mike.pos.y = mike.pos.y - 5;
+    }
+    if (evt.key === 39) {
+        mike.pos.x = mike.pos.x + 5;
+    }
+    if (evt.key === 40) {
+        mike.pos.y = mike.pos.y + 5;
+    }
+    console.log(evt.key);
+})
+
+let interval = {
+    left: null,
+    right: null,
+    up: null,
+    down: null,
+}
+
+game.input.keyboard.on("hold", (evt) => {
+    if (evt.key === 37 && interval.left == null) {
+        interval.left = setInterval(() => {
+            mike.pos.x = mike.pos.x - 5;
+        }, 10)
+    }
+    if (evt.key === 38 && interval.up == null) {
+        interval.up = setInterval(() => {
+            mike.pos.y = mike.pos.y - 5;
+        }, 10)
+    }
+    if (evt.key === 39 && interval.right == null) {
+        interval.right = setInterval(() => {
+            mike.pos.x = mike.pos.x + 5;
+        }, 10)
+    }
+    if (evt.key === 40 && interval.down == null) {
+        interval.down = setInterval(() => {
+            mike.pos.y = mike.pos.y + 5;
+        }, 10)
+    }
+})
+game.input.keyboard.on("release", (evt) => {
+    if (evt.key === 37) {
+        clearInterval(interval.left);
+        interval.left = null;
+    }
+    if (evt.key === 38) {
+        clearInterval(interval.up);
+        interval.up = null;
+    }
+    if (evt.key === 39) {
+        clearInterval(interval.right);
+        interval.right = null;
+    }
+    if (evt.key === 40) {
+        clearInterval(interval.down);
+        interval.down = null;
+    }
+})
+
+mike.body.collider.type = CollisionType.Active;
+han.body.collider.type = CollisionType.Active;
+
+game.add(mike);
+game.add(han);
+
+
+game.start()
